411efa01b49d32ebf696985b0fa04b9baf87c865
ddderi <maxime830@live.fr>
2022-01-13 07:06:13 +0100

    commit tree 20d77910521124a7ed04986d36199fc8d6cdcf85

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    return array.filter(n => n === string.toLowerCase() || n === string)
+       
+   }
+
+
+   function fuzzyMatch(array, string) {
+    return array.filter(n => n[0]+n[1] === string)
+}
+ 
+function matchName(array, string) {
+    return array.filter(n => n.name === string)
+}
